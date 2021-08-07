9854cf99cb755442b87c63336735497c086e9920
marta_pg <marta.pineda.g@gmail.com>
2021-08-07 21:50:39 +0300

    updated phonetic

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -6,7 +6,7 @@ import ReactAudioPlayer from "react-audio-player";
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank"></a>
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">.</a>
       <div className="phoneticText">/{props.phonetic.text}/</div>
       <br />
       <div className="player">
