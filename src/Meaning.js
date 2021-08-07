4a4705958f2f9a86cd94ed82acff2ffee0ebf84b
marta_pg <marta.pineda.g@gmail.com>
2021-08-07 17:42:34 +0300

    update classes

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,12 +5,12 @@ import React from "react";
 export default function Meaning(props) {
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech} </h3>
+      <h3 className="searched-word">{props.meaning.partOfSpeech} </h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
             <p>
-              <strong>{definition.defintion}</strong>
+              <strong className="definition">{definition.definition}</strong>
               <em> {definition.example}</em>
             </p>
           </div>
