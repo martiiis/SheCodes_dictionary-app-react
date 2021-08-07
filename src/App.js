13e3053dbc964f7874aadcc29b80ea879f95c053
marta_pg <marta.pineda.g@gmail.com>
2021-08-07 16:24:03 +0300

    added search engine + alert

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import "./App.css";
 
 import "bootstrap/dist/css/bootstrap.css";
 import Footer from "./Footer.js";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <div className="container dictionary">
+      <div className="container-header">
         <h1>Dictionary</h1>
+      </div>
+      <div className="container-dictionary">
+        <Dictionary />
       </div>
       <Footer />
     </div>
