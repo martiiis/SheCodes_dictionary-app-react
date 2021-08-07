76c81f61cf1eba57dc731e6af40ef59cac154c8b
marta_pg <marta.pineda.g@gmail.com>
2021-08-07 16:49:55 +0300

    api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,23 @@
 /** @format */
 
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`⏳ Searching for: ${keyword}`);
+    //documentation: https://dictionaryapi.dev/
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
+
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
